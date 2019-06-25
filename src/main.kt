d4bdb60d8ef8408e7caece947e931b90dc96ae03
Daniel Alvarez <a3dany@gmail.com>
2019-06-25 02:15:01 -0400

    Alternative with DDD concepts

--- src/main.kt
+++ src/main.kt
@@ -1,13 +1,81 @@
 data class Cart(val id: Int) {
     var items: List<Item> = mutableListOf()
 
-    val total: Float
+    val total: Money
         get () {
-            return items.map { it.price }.sum()
+            return Money(items.map { it.price.defaultValue }.sum())
         }
 }
 
-data class Item(val id: Int, var price: Float, var cat: String, var weight: Float)
+data class Item(val id: Int, var price: Money, var category: ItemCategory, var weight: Float) {
+
+    fun costForWeight(cost: Money): Money {
+        return Money(weight / 1000 * cost.defaultValue)
+    }
+}
+
+data class Money(var value: Float, val currency: String = USD) {
+    companion object {
+        const val USD = "USD"
+        val ZERO = Money(0F, USD)
+    }
+
+    val defaultValue: Float
+        get() {
+            if (currency == USD) {
+                return value
+            }
+            return 0F // TODO: Implement conversion
+        }
+
+
+    fun lessThan(amount: Money): Boolean {
+        if (value <= amount.value && currency == amount.currency) {
+            return true
+        }
+        return false
+    }
+
+    fun add(money: Money) {
+        if (currency == money.currency) {
+            value += money.value
+        }
+    }
+}
+
+enum class ItemCategory {
+    C, Q
+}
+
+enum class ShippingOption {
+    NORMAL {
+        override fun costForItem(item: Item): Money {
+            if (item.category == ItemCategory.C) {
+                return Money(2.99F)
+            }
+            if (item.category == ItemCategory.Q) {
+                return item.costForWeight(Money(2.99F))
+            }
+            return Money.ZERO
+        }
+    },
+    PRIORITY {
+        override fun costForItem(item: Item): Money {
+            if (item.category == ItemCategory.C) {
+                return Money(4.99F)
+            }
+            if (item.category == ItemCategory.Q) {
+                return item.costForWeight(Money(2.99F))
+            }
+            return Money.ZERO
+        }
+    };
+
+    abstract fun costForItem(item: Item): Money
+}
+
+val AMOUNT_FREE_SHIPPING = Money(100F)
+val BASE_SHIPMENT_COST = Money(4.9F)
 
 var carts: MutableList<Cart> = mutableListOf()
 
@@ -15,43 +83,23 @@ fun main() {
 
     val cart1 = Cart(1)
     cart1.items = listOf(
-        Item(111, 2.5F, "C", 1F),
-        Item(222, 25.8F, "Q", 2F)
+        Item(111, Money(2.5F), ItemCategory.C, 1F),
+        Item(222, Money(25.8F), ItemCategory.Q, 2F)
     )
     carts.add(cart1)
 
-    val shippingCost = process(1, 1)
+    val shippingCost = calculateShippingCost(cart1, ShippingOption.NORMAL)
 
     print(shippingCost)
 }
 
-fun process(cartId: Int, option: Int): Float {
-    val cart = carts.first { it.id == cartId }
-
-    var cost = 0F
-    if (cart.total <= 100) {
-        cost = 4.99F
-
-        if (option == 1) {
-            for (i in cart.items) {
-                if (i.cat == "C") {
-                    cost += 2.99F
-                }
-                if (i.cat == "Q") {
-                    cost += i.weight / 1000 * 2.99F
-                }
-            }
+fun calculateShippingCost(cart: Cart, shippingOption: ShippingOption): Money {
+    if (cart.total.lessThan(AMOUNT_FREE_SHIPPING)) {
+        val shippingCost = BASE_SHIPMENT_COST
+        for (item in cart.items) {
+            shippingCost.add(shippingOption.costForItem(item))
         }
-        if (option == 2) {
-            for (i in cart.items) {
-                if (i.cat == "C") {
-                    cost += 4.99F
-                }
-                if (i.cat == "Q") {
-                    cost += i.weight / 1000 * 2.99F
-                }
-            }
-        }
+        return shippingCost
     }
-    return cost
+    return Money.ZERO
 }
